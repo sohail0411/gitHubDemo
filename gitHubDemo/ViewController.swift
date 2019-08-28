8a49da4298373fa165c3342c4ff6256c1f48c807
Sohail Shaik <sohail0411@achiever-36.local>
2019-08-28 12:35:54 +0530

    Initial Commit

---
+++ gitHubDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  gitHubDemo
+//
+//  Created by Sohail Shaik on 8/28/19.
+//  Copyright © 2019 Sohail Shaik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
